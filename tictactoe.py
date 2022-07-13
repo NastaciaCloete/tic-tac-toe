14d81b14a9fc476511516f0514fe0c4f6a9b1db2
nastacia <nastacia.cloete@deepcrawl.com>
2022-07-13 16:27:30 +0100

    failed singleplayer

--- tictactoe.py
+++ tictactoe.py
@@ -20,10 +20,16 @@ if player1 == "O" or player1 == "naughts" or player1 =="o":
 elif player1 == "X" or player1 == "crosses" or player1 == "x":
     player1 = "X"
 
+if player1 == "O":
+    computer = "X"
+elif player1 == "X":
+    computer = "O"
+
 def game():
     
         turn = player1
         count = 0
+        turn1 = computer
 
         for i in range(10):
             printBoard(theBoard)
@@ -37,6 +43,259 @@ def game():
             else:
                 print ("that place is already filled. \nMove to which place?")
                 continue 
+
+            #square 1
+            if count >= 1:
+                if theBoard['1'] == player1:
+                    print("the computer chooses:")
+                    if theBoard['5'] == ' ':
+                        theBoard['5'] = turn1
+                    elif theBoard['5'] != ' ':
+                        if theBoard['9'] == ' ':
+                            theBoard['9'] = turn1
+                        elif theBoard['9'] != ' ':
+                            if theBoard['3'] == ' ':
+                                theBoard['3'] = turn1
+                            elif theBoard['3'] != ' ':
+                                if theBoard['7'] == ' ':
+                                    theBoard['7'] = turn1
+                                elif theBoard['7'] != ' ':
+                                    if theBoard['8'] == ' ':
+                                        theBoard['8'] = turn1
+                                    elif theBoard['8'] != ' ':
+                                        if theBoard['6'] == ' ':
+                                            theBoard['6'] = turn1
+                                        elif theBoard['6'] != ' ':
+                                            if theBoard['4'] == ' ':
+                                                theBoard['4'] = turn1
+                                            elif theBoard['4'] != ' ':
+                                                theBoard['2'] = turn1
+            printBoard (theBoard)
+
+            #square 2
+            if count >= 1:
+                if theBoard['2'] == player1:
+                    print("the computer chooses:")
+                    if theBoard['5'] == ' ':
+                        theBoard['5'] = turn1
+                    elif theBoard['5'] != ' ':
+                        if theBoard['9'] == ' ':
+                            theBoard['9'] = turn1
+                        elif theBoard['9'] != ' ':
+                            if theBoard['3'] == ' ':
+                                theBoard['3'] = turn1
+                            elif theBoard['3'] != ' ':
+                                if theBoard['7'] == ' ':
+                                    theBoard['7'] = turn1
+                                elif theBoard['7'] != ' ':
+                                    if theBoard['1'] == ' ':
+                                        theBoard['1'] = turn1
+                                    elif theBoard['1'] != ' ':
+                                        if theBoard['8'] == ' ':
+                                            theBoard['8'] = turn1
+                                        elif theBoard['8'] != ' ':
+                                            if theBoard['4'] == ' ':
+                                                theBoard['4'] = turn1
+                                            elif theBoard['4'] != ' ':
+                                                theBoard['6'] = turn1
+            printBoard (theBoard)
+
+    
+            #square 3
+            if count >= 1:
+                if theBoard['3'] == player1:
+                    print("the computer chooses:")
+                    if theBoard['5'] == ' ':
+                        theBoard['5'] = turn1
+                    elif theBoard['5'] != ' ':
+                        if theBoard['9'] == ' ':
+                            theBoard['9'] = turn1
+                        elif theBoard['9'] != ' ':
+                            if theBoard['1'] == ' ':
+                                theBoard['1'] = turn1
+                            elif theBoard['1'] != ' ':
+                                if theBoard['7'] == ' ':
+                                    theBoard['7'] = turn1
+                                elif theBoard['7'] != ' ':
+                                    if theBoard['8'] == ' ':
+                                        theBoard['8'] = turn1
+                                    elif theBoard['8'] != ' ':
+                                        if theBoard['6'] == ' ':
+                                            theBoard['6'] = turn1
+                                        elif theBoard['6'] != ' ':
+                                            if theBoard['4'] == ' ':
+                                                theBoard['4'] = turn1
+                                            elif theBoard['4'] != ' ':
+                                                theBoard['2'] = turn1
+            printBoard (theBoard)
+
+            #square 4
+            if count >= 1:
+                if theBoard['4'] == player1:
+                    print("the computer chooses:")
+                    if theBoard['5'] == ' ':
+                        theBoard['5'] = turn1
+                    elif theBoard['5'] != ' ':
+                        if theBoard['9'] == ' ':
+                            theBoard['9'] = turn1
+                        elif theBoard['9'] != ' ':
+                            if theBoard['3'] == ' ':
+                                theBoard['3'] = turn1
+                            elif theBoard['3'] != ' ':
+                                if theBoard['7'] == ' ':
+                                    theBoard['7'] = turn1
+                                elif theBoard['7'] != ' ':
+                                    if theBoard['1'] == ' ':
+                                        theBoard['1'] = turn1
+                                    elif theBoard['1'] != ' ':
+                                        if theBoard['6'] == ' ':
+                                            theBoard['6'] = turn1
+                                        elif theBoard['6'] != ' ':
+                                            if theBoard['8'] == ' ':
+                                                theBoard['8'] = turn1
+                                            elif theBoard['8'] != ' ':
+                                                theBoard['2'] = turn1
+            printBoard (theBoard)
+
+            #square 5
+            if count >= 1:
+                if theBoard['5'] == player1:
+                    print("the computer chooses:")
+                    if theBoard['9'] == ' ':
+                        theBoard['9'] = turn1
+                    elif theBoard['9'] != ' ':
+                        if theBoard['3'] == ' ':
+                            theBoard['3'] = turn1
+                        elif theBoard['3'] != ' ':
+                            if theBoard['7'] == ' ':
+                                theBoard['7'] = turn1
+                            elif theBoard['7'] != ' ':
+                                if theBoard['1'] == ' ':
+                                    theBoard['1'] = turn1
+                                elif theBoard['1'] != ' ':
+                                    if theBoard['6'] == ' ':
+                                        theBoard['6'] = turn1
+                                    elif theBoard['6'] != ' ':
+                                        if theBoard['8'] == ' ':
+                                            theBoard['8'] = turn1
+                                        elif theBoard['8'] != ' ':
+                                            if theBoard['4'] == ' ':
+                                                theBoard['4'] = turn1
+                                            elif theBoard['4'] != ' ':
+                                                theBoard['2'] = turn1
+            printBoard (theBoard)
+
+            #square 6
+            if count >= 1:
+                if theBoard['6'] == player1:
+                    print("the computer chooses:")
+                    if theBoard['5'] == ' ':
+                        theBoard['5'] = turn1
+                    elif theBoard['5'] != ' ':
+                        if theBoard['9'] == ' ':
+                            theBoard['9'] = turn1
+                        elif theBoard['9'] != ' ':
+                            if theBoard['3'] == ' ':
+                                theBoard['3'] = turn1
+                            elif theBoard['3'] != ' ':
+                                if theBoard['7'] == ' ':
+                                    theBoard['7'] = turn1
+                                elif theBoard['7'] != ' ':
+                                    if theBoard['1'] == ' ':
+                                        theBoard['1'] = turn1
+                                    elif theBoard['1'] != ' ':
+                                        if theBoard['8'] == ' ':
+                                            theBoard['8'] = turn1
+                                        elif theBoard['8'] != ' ':
+                                            if theBoard['4'] == ' ':
+                                                theBoard['4'] = turn1
+                                            elif theBoard['4'] != ' ':
+                                                theBoard['2'] = turn1
+            printBoard (theBoard)
+
+            #square 7
+            if count >= 1:
+                if theBoard['7'] == player1:
+                    print("the computer chooses:")
+                    if theBoard['5'] == ' ':
+                        theBoard['5'] = turn1
+                    elif theBoard['5'] != ' ':
+                        if theBoard['9'] == ' ':
+                            theBoard['9'] = turn1
+                        elif theBoard['9'] != ' ':
+                            if theBoard['3'] == ' ':
+                                theBoard['3'] = turn1
+                            elif theBoard['3'] != ' ':
+                                if theBoard['1'] == ' ':
+                                    theBoard['1'] = turn1
+                                elif theBoard['1'] != ' ':
+                                    if theBoard['8'] == ' ':
+                                        theBoard['8'] = turn1
+                                    elif theBoard['8'] != ' ':
+                                        if theBoard['6'] == ' ':
+                                            theBoard['6'] = turn1
+                                        elif theBoard['6'] != ' ':
+                                            if theBoard['4'] == ' ':
+                                                theBoard['4'] = turn1
+                                            elif theBoard['4'] != ' ':
+                                                theBoard['2'] = turn1
+            printBoard (theBoard)
+
+            #square 8
+            if count >= 1:
+                if theBoard['8'] == player1:
+                    print("the computer chooses:")
+                    if theBoard['5'] == ' ':
+                        theBoard['5'] = turn1
+                    elif theBoard['5'] != ' ':
+                        if theBoard['9'] == ' ':
+                            theBoard['9'] = turn1
+                        elif theBoard['9'] != ' ':
+                            if theBoard['3'] == ' ':
+                                theBoard['3'] = turn1
+                            elif theBoard['3'] != ' ':
+                                if theBoard['7'] == ' ':
+                                    theBoard['7'] = turn1
+                                elif theBoard['7'] != ' ':
+                                    if theBoard['1'] == ' ':
+                                        theBoard['1'] = turn1
+                                    elif theBoard['1'] != ' ':
+                                        if theBoard['6'] == ' ':
+                                            theBoard['6'] = turn1
+                                        elif theBoard['6'] != ' ':
+                                            if theBoard['4'] == ' ':
+                                                theBoard['4'] = turn1
+                                            elif theBoard['4'] != ' ':
+                                                theBoard['2'] = turn1
+            printBoard (theBoard)
+
+            #square 9
+            if count >= 1:
+                if theBoard['9'] == player1:
+                    print("the computer chooses:")
+                    if theBoard['5'] == ' ':
+                        theBoard['5'] = turn1
+                    elif theBoard['5'] != ' ':
+                        if theBoard['7'] == ' ':
+                            theBoard['7'] = turn1
+                        elif theBoard['7'] != ' ':
+                            if theBoard['3'] == ' ':
+                                theBoard['3'] = turn1
+                            elif theBoard['3'] != ' ':
+                                if theBoard['1'] == ' ':
+                                    theBoard['1'] = turn1
+                                elif theBoard['1'] != ' ':
+                                    if theBoard['8'] == ' ':
+                                        theBoard['8'] = turn1
+                                    elif theBoard['8'] != ' ':
+                                        if theBoard['6'] == ' ':
+                                            theBoard['6'] = turn1
+                                        elif theBoard['6'] != ' ':
+                                            if theBoard['4'] == ' ':
+                                                theBoard['4'] = turn1
+                                            elif theBoard['4'] != ' ':
+                                                theBoard['2'] = turn1
+            printBoard (theBoard)
 
             if count >= 5:
                 if theBoard['7'] == theBoard ['8'] == theBoard['9'] != ' ':
@@ -84,11 +343,6 @@ def game():
                 print ("\n Game Over.\n")
                 print("It's a Tie!")
                 break
-
-            if turn == "X":
-                turn = "O"
-            else:
-                turn = "X"
 
         restart = input("Do you want to play again? (y/n)")
         if restart == "y" or restart == "Y":
